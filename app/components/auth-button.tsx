4a695946ce2193615c5281dd9b4efe0db7c0dec5
Anthony Pecchillo <anthonypecchillo@gmail.com>
2024-10-16 13:00:00 -0700

    feat: update temporary AuthButton component styles

--- app/components/auth-button.tsx
+++ app/components/auth-button.tsx
@@ -6,8 +6,8 @@ import type { loader as rootLoader } from "~/root";
 export function AuthButton() {
   const rootLoaderData = useRouteLoaderData<typeof rootLoader>("root");
 
-  const boltbetz = rootLoaderData?.session?.boltbetzUser;
-  const isLoggedIn = Boolean(boltbetz);
+  const boltbetzUser = rootLoaderData?.session?.boltbetzUser;
+  const isLoggedIn = Boolean(boltbetzUser);
 
   const formActionRoute = isLoggedIn ? "/api/auth/logout" : "/api/auth/login";
   const buttonText = isLoggedIn ? "Sign Out" : "Sign In";
@@ -16,9 +16,9 @@ export function AuthButton() {
     <Form action={route(formActionRoute)} method="POST">
       <button
         type="submit"
-        className={clsx("rounded-md px-6 py-2 font-semibold text-white", {
+        className={clsx("mt-10 rounded-md px-6 py-2 font-semibold text-white", {
           "bg-red-500 hover:bg-red-600": isLoggedIn,
-          "bg-blue-700 hover:bg-blue-800": !isLoggedIn,
+          "bg-green-600 hover:bg-green-700": !isLoggedIn,
         })}
       >
         {buttonText}
